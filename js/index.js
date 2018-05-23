0e458d872a4af44548199b21284016936ca4a8a9
Dmitry <dskhromin@gmail.com>
2018-05-23 22:00:13 +0500

    Добавлен переходы на другие страницы

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
 // JavaScript Document
 
+function jackal() {
+	
+	top.location.href='html/jackal.html';
+	
+}
+
+function imaginarium() {
+	
+	top.location.href='html/imaginarium.html';
+	
+}
+
+function lotto() {
+	
+	top.location.href='html/lotto.html';
+	
+}
+
+function dominoes() {
+	
+	top.location.href='html/dominoes.html';
+	
+}
+
+function equivocation() {
+	
+	top.location.href='html/equivocation.html';
+	
+}
